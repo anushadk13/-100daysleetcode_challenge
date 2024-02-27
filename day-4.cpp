bb8de3282eaabcd1db9513eaa46051258edf50d0
Anusha D K <122234698+anushadk13@users.noreply.github.com>
2024-02-27 17:19:08 +0530

    Create day-4.cpp

---
+++ day-4.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        calculateDiameter(root, diameter);
+        return diameter;
+    }
+
+
+    int calculateDiameter(TreeNode* root, int& diameter) {
+        if (!root) {
+            return 0;
+        }
+
+        // Recursively calculate the height of the left and right subtrees
+        int leftHeight = calculateDiameter(root->left, diameter);
+        int rightHeight = calculateDiameter(root->right, diameter);
+
+        // Update the diameter if the current node's diameter is greater
+        diameter = max(diameter, leftHeight + rightHeight);
+
+        // Return the height of the current subtree
+        return 1 + max(leftHeight, rightHeight);
+    }
+};
+    
